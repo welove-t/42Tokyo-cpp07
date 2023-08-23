ccafb738a6e46500897088269e976ae9acf531b5
love <tlove.tky@gmail.com>
2023-08-23 10:25:44 +0900

    ex00: 関数テンプレートの実装

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -1,2 +1,21 @@
 #include <iostream>
 
+template <typename T>
+void	swap(T& a, T& b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T>
+T min(T a, T b)
+{
+	return a < b ? a : b;
+}
+
+template <typename T>
+T max(T a, T b)
+{
+	return a > b ? a : b;
+}
